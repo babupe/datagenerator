5fcb48555e07012306a5d9eab8f493a343b13cb8
Elumalai <babupe@a82066444001.ant.amazon.com>
2015-02-06 21:44:47 -0800

    modify data generator to have only 1 copy running at a time

--- gen_data.py
+++ gen_data.py
@@ -7,18 +7,19 @@ logtype = ['crash','notif','download']
 devinfo = ['iphone-ios','android-kitkat','fire-fireos']
 appnm = ['app1','app2','app3','app4']
 mtime = os.stat('/home/ec2-user/gen_data.py')[8]
-while 1:
-	if mtime != os.stat('/home/ec2-user/gen_data.py')[8]:
-		try:
-			f.close()
-		except:
-			print 'None'
-		sys.exit(0)
-	currTime = str(datetime.now())
-	fname = "/fs-data/applogs-"+currTime[:4]+currTime[5:7]+currTime[8:10]+currTime[11:13]+currTime[14:16]+".log"
-	f = open(fname,'a')
-	while str(currTime[14:16]) == str(datetime.now())[14:16]:
-		currTm = str(datetime.now())
-		eventTmstp = currTime[:4]+'-'+currTime[5:7]+'-'+currTime[8:10]+' '+currTime[11:13]+':'+currTime[14:16]+':'+currTime[17:19]
-		f.write("AppName:"+appnm[randint(0,len(appnm)-1)]+",DeviceInfo:"+devinfo[randint(0,len(devinfo)-1)]+",logType:"+logtype[randint(0,len(logtype)-1)]+",SWRevision:"+swrevision[randint(0,len(swrevision)-1)]+",eventTimestamp:"+eventTmstp+"\n")
-	f.close()
+if int(os.popen('ps -ef|grep gen_data.py|grep -v grep|wc -l').read()) == 1:
+	while 1:
+		if mtime != os.stat('/home/ec2-user/gen_data.py')[8]:
+			try:
+				f.close()
+			except:
+				print 'None'
+			sys.exit(0)
+		currTime = str(datetime.now())
+		fname = "/fs-data/applogs-"+currTime[:4]+currTime[5:7]+currTime[8:10]+currTime[11:13]+currTime[14:16]+".log"
+		f = open(fname,'a')
+		while str(currTime[14:16]) == str(datetime.now())[14:16]:
+			currTm = str(datetime.now())
+			eventTmstp = currTime[:4]+'-'+currTime[5:7]+'-'+currTime[8:10]+' '+currTime[11:13]+':'+currTime[14:16]+':'+currTime[17:19]
+			f.write("AppName:"+appnm[randint(0,len(appnm)-1)]+",DeviceInfo:"+devinfo[randint(0,len(devinfo)-1)]+",logType:"+logtype[randint(0,len(logtype)-1)]+",SWRevision:"+swrevision[randint(0,len(swrevision)-1)]+",eventTimestamp:"+eventTmstp+"\n")
+		f.close()
--- write_to_s3.py
+++ write_to_s3.py
@@ -10,24 +10,24 @@ def get_region():
     metadata = get_instance_metadata()
     return metadata['placement']['availability-zone'][:-1]
 
-
-s3_conn = boto.connect_s3(get_region())
-buckt = s3_conn.get_bucket('myapplogsbabupe') 
-mtime = os.stat('/home/ec2-user/write_to_s3.py')[8]
-while 1:
-	if mtime != os.stat('/home/ec2-user/write_to_s3.py')[8]:
-		try:
-			f.close()
-		except:
-			print 'None'
-		sys.exit(0)
-	currTime = str(datetime.now())
-	mincomp = int(currTime[14:16]) -1
-	if len(str(mincomp)) == 1:
-		mincomp = '0'+str(mincomp)
-	if os.path.isfile('/fs-data/applogs-'+currTime[:4]+currTime[5:7]+currTime[8:10]+currTime[11:13]+str(mincomp)+'.log'):
-		k = Key(buckt)
-		k.key = 'applogs/'+currTime[:4]+'/'+currTime[5:7]+'/'+currTime[8:10]+'/'+currTime[11:13]+'/'+str(mincomp)+'.log'
-		k.set_contents_from_filename('/fs-data/applogs-'+currTime[:4]+currTime[5:7]+currTime[8:10]+currTime[11:13]+str(mincomp)+'.log')	
-		os.remove('/fs-data/applogs-'+currTime[:4]+currTime[5:7]+currTime[8:10]+currTime[11:13]+str(mincomp)+'.log')
-	time.sleep(10)
+if int(os.popen('ps -ef|grep gen_data.py|grep -v grep|wc -l').read()) == 1:
+	s3_conn = boto.connect_s3(get_region())
+	buckt = s3_conn.get_bucket('myapplogsbabupe') 
+	mtime = os.stat('/home/ec2-user/write_to_s3.py')[8]
+	while 1:
+		if mtime != os.stat('/home/ec2-user/write_to_s3.py')[8]:
+			try:
+				f.close()
+			except:
+				print 'None'
+			sys.exit(0)
+		currTime = str(datetime.now())
+		mincomp = int(currTime[14:16]) -1
+		if len(str(mincomp)) == 1:
+			mincomp = '0'+str(mincomp)
+		if os.path.isfile('/fs-data/applogs-'+currTime[:4]+currTime[5:7]+currTime[8:10]+currTime[11:13]+str(mincomp)+'.log'):
+			k = Key(buckt)
+			k.key = 'applogs/'+currTime[:4]+'/'+currTime[5:7]+'/'+currTime[8:10]+'/'+currTime[11:13]+'/'+str(mincomp)+'.log'
+			k.set_contents_from_filename('/fs-data/applogs-'+currTime[:4]+currTime[5:7]+currTime[8:10]+currTime[11:13]+str(mincomp)+'.log')	
+			os.remove('/fs-data/applogs-'+currTime[:4]+currTime[5:7]+currTime[8:10]+currTime[11:13]+str(mincomp)+'.log')
+		time.sleep(10)
